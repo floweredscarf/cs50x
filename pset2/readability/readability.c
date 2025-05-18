1e372ede2143679242a37a82dd215b3f6ba43a99
floweredscarf <floweredscarf@gmail.com>
2025-05-18 15:24:38 +0800

    Complete Readability

--- pset2/readability/readability.c
+++ pset2/readability/readability.c
@@ -19,21 +19,72 @@ int main(void)
     int sentences = count_sentences(text);
 
     // Compute the Coleman-Liau index
+    // L is the average number of letters per 100 words in the text: that is,
+    // the number of letters divided by the number of words, all multiplied by 100.
+    float L = (float) letters / words * 100;
+    // S is the average number of sentences per 100 words in the text: that is,
+    // the number of sentences divided by the number of words, all multiplied by 100.
+    float S = (float) sentences / words * 100;
+    int index = round(0.0588 * L - 0.296 * S - 15.8);
 
     // Print the grade level
+    if (index >= 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else if (index < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else
+    {
+        printf("Grade %i\n", index);
+    }
 }
 
 int count_letters(string text)
 {
     // Return the number of letters in text
+    int count = 0;
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        if (isalpha(text[i]))
+        {
+            count++;
+        }
+    }
+    return count;
 }
 
 int count_words(string text)
 {
     // Return the number of words in text
+    // Assume that a sentence:
+    // Will contain at least one word;
+    // Will not start or end with a space; and
+    // Will not have multiple spaces in a row.
+    int count = 1;
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        if (text[i] == ' ')
+        {
+            count++;
+        }
+    }
+    return count;
 }
 
 int count_sentences(string text)
 {
     // Return the number of sentences in text
+    // Consider any sequence of characters that ends with a . or a ! or a ? to be a sentence.
+    int count = 0;
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        if (text[i] == '.' || text[i] == '!' || text[i] == '?')
+        {
+            count++;
+        }
+    }
+    return count;
 }

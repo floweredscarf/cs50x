2943c519379fb26ff683d6e47e2c4c9ae9ea2c2e
floweredscarf <floweredscarf@gmail.com>
2025-05-21 12:18:22 +0800

    implement grayscale

--- pset4/filter-less/helpers.c
+++ pset4/filter-less/helpers.c
@@ -1,8 +1,22 @@
 #include "helpers.h"
+#include <math.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    // Loop over all pixels
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            // Take average of red, green, and blue
+            int average = round((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3.0);
+            // Update pixel values
+            image[i][j].rgbtBlue = average;
+            image[i][j].rgbtGreen = average;
+            image[i][j].rgbtRed = average;
+        }
+    }
     return;
 }
 

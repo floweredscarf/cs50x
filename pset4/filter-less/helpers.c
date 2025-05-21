a1a11a127be86f8fc2d6c18cb5660c86aa3b5f8e
floweredscarf <floweredscarf@gmail.com>
2025-05-21 13:09:58 +0800

    implement blur

--- pset4/filter-less/helpers.c
+++ pset4/filter-less/helpers.c
@@ -65,5 +65,40 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    // Create a copy of image
+    RGBTRIPLE copy[height][width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            copy[i][j] = image[i][j];
+        }
+    }
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int sumBlue = 0;
+            int sumGreen = 0;
+            int sumRed = 0;
+            int count = 0;
+            for (int di = -1; di <= 1; di++)
+            {
+                for (int dj = -1; dj <= 1; dj++)
+                {
+                    if (i + di >= 0 && i + di < height && j + dj >= 0 && j + dj < width)
+                    {
+                        sumBlue += copy[i + di][j + dj].rgbtBlue;
+                        sumGreen += copy[i + di][j + dj].rgbtGreen;
+                        sumRed += copy[i + di][j + dj].rgbtRed;
+                        count++;
+                    }
+                }
+            }
+            image[i][j].rgbtBlue = round((float) sumBlue / count);
+            image[i][j].rgbtGreen = round((float) sumGreen / count);
+            image[i][j].rgbtRed = round((float) sumRed / count);
+        }
+    }
     return;
 }

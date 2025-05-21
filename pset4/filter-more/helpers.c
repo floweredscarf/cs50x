81ce7c148acf00255acdaf243ee5a733de5a8d31
floweredscarf <floweredscarf@gmail.com>
2025-05-21 13:48:17 +0800

    implement grayscale reflect blur

--- pset4/filter-more/helpers.c
+++ pset4/filter-more/helpers.c
@@ -1,20 +1,80 @@
 #include "helpers.h"
+#include <math.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    // Loop over all pixels
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            // Take average of red, green, and blue
+            int average = round((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3.0);
+            // Update pixel values
+            image[i][j].rgbtBlue = average;
+            image[i][j].rgbtGreen = average;
+            image[i][j].rgbtRed = average;
+        }
+    }
     return;
 }
 
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    // Loop over all pixels
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width / 2; j++)
+        {
+            // Swap pixels
+            RGBTRIPLE tmp = image[i][j];
+            image[i][j] = image[i][width - 1 - j];
+            image[i][width - 1 - j] = tmp;
+        }
+    }
     return;
 }
 
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    // Create a copy of image
+    RGBTRIPLE copy[height][width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            copy[i][j] = image[i][j];
+        }
+    }
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int sumBlue = 0;
+            int sumGreen = 0;
+            int sumRed = 0;
+            int count = 0;
+            for (int di = -1; di <= 1; di++)
+            {
+                for (int dj = -1; dj <= 1; dj++)
+                {
+                    if (i + di >= 0 && i + di < height && j + dj >= 0 && j + dj < width)
+                    {
+                        sumBlue += copy[i + di][j + dj].rgbtBlue;
+                        sumGreen += copy[i + di][j + dj].rgbtGreen;
+                        sumRed += copy[i + di][j + dj].rgbtRed;
+                        count++;
+                    }
+                }
+            }
+            image[i][j].rgbtBlue = round((float) sumBlue / count);
+            image[i][j].rgbtGreen = round((float) sumGreen / count);
+            image[i][j].rgbtRed = round((float) sumRed / count);
+        }
+    }
     return;
 }
 
